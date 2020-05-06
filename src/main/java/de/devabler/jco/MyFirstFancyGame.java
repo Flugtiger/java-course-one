99d437747cf84c757b662208b8ab7856497256e9
Alexander Lehmann <a.lehm@gmx.de>
2020-05-06 10:58:08 +0000

    lektion 1

--- src/main/java/de/devabler/jco/MyFirstFancyGame.java
+++ src/main/java/de/devabler/jco/MyFirstFancyGame.java
@@ -1,16 +1,28 @@
 package de.devabler.jco;
 
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 
 /**
+ * 
  * Mein erstes krasses Spiel!
  */
 public class MyFirstFancyGame {
 
     void runGame(Screen screen, TextGraphics textGraphics) throws Exception {
-        textGraphics.putString(0,0, "Hallo Welt!");
-        screen.refresh();
-        Thread.sleep(2000);
+      
+        var y = screen.getTerminalSize().getRows() -1; 
+        var x = screen.getTerminalSize().getColumns() /2;
+        while(true) {
+
+            var eingabe = screen.pollInput();
+            if(eingabe != null && eingabe.getKeyType() == KeyType.ArrowLeft) {
+                x = x-1;
+            }
+            textGraphics.putString(x,y, " -/\\- ");
+            screen.refresh();
+            Thread.sleep(100);
+        }
     }
 }
